96a253ddbbc2c42af7d00e7fb599112ef78aa33e
Leonardo <leonardo.cordeiro@virtus.ufcg.edu.br>
2017-03-30 10:36:00 -0300

    Atualizando view

--- untitled/serverPass/views.py
+++ untitled/serverPass/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework import status
+from keystoneauth1 import loading
+from keystoneauth1 import session
 
 from serializers import UserSerializer
 
@@ -11,11 +13,18 @@ from serializers import UserSerializer
 
 # ViewSets define the view behavior.
 from rest_framework import viewsets
+from heatclient import client
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+tenant_id = 'b363706f891f48019483f8bd6503c54b'
+heat_url = 'http://heat.example.org:8004/v1/%s' % tenant_id
+auth_token = '3bcc3d3a03f44e3d8377f9247b0ad155'
+
+
 
 @api_view(['GET'])
 @renderer_classes((JSONRenderer,))
@@ -36,8 +45,18 @@ def app(request):
         "TRANSPORTE",
         "SEGURANCA",
         "OUTROS"]
-
-    return Response({"Requisicao revebida"}, status=status.HTTP_200_OK)
+    loader = loading.get_plugin_loader('password')
+    auth = loader.load_from_options(auth_url='http://180.10.10.51:35357/v3',
+    username='admin',
+    password='ADMIN_PASS',
+    user_domain_name='default',
+    project_id='04a4ea69b5054d0b855f09c6ba275b06')
+    sess = session.Session(auth=auth)
+    heat = client.Client('1', session=sess)
+    stacks = heat.stacks
+    stacks.create()
+    #print(next(heat.stacks.list()))
+    return Response({"ok"}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
